8c90fe44fcdac49a1eb3687afc43d9c1950ab460
Paul Riffard <paul.riffard@gmail.com>
2020-02-20 09:45:54 +0100

    connection_firebase

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  apiKey: "AIzaSyBxdco3FhVMujz7wAd6o42pF1lg0hYJ8VA",
+  authDomain: "movie-tracker-webapp.firebaseapp.com",
+  databaseURL: "https://movie-tracker-webapp.firebaseio.com"
+};
