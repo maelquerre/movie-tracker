b6e6b6f662f268f531750c42ee270e6102124265
Paul Riffard <paul.riffard@gmail.com>
2020-05-20 20:19:12 +0200

    remove untracked

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  apiKey: "AIzaSyBxdco3FhVMujz7wAd6o42pF1lg0hYJ8VA",
-  authDomain: "movie-tracker-webapp.firebaseapp.com",
-  databaseURL: "https://movie-tracker-webapp.firebaseio.com"
-};
